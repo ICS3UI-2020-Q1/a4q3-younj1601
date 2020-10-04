19bb993be7393a2763960cf5f0d79a3e6c09408e
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 15:18:03 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Calculate the sum from one to inputed number
+ * @author Jackson 
  */
 public class Main {
 
@@ -9,7 +10,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //scanner for input
+    Scanner input = new Scanner(System.in);
+
+    //get the interger
+    System.out.println("Please enter an integer to count down to");
+    int num = input.nextInt();
+    int count = 0;
+    int sum = 0;
+
+    while(count < num){
+      count = count + 1;
+      sum = sum + count;
+      
+    }
+    System.out.println("The sum from 1 - " + num + " is " + sum);
   }
 }
